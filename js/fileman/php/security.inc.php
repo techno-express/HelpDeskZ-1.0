1e6c70680187ca1c2110ba46bdbd727196d26d71
Filou <philip.tschiemer@smart-home-technology.ch>
2016-02-15 16:39:17 +0100

    integration of fileman filebrowser

---
+++ js/fileman/php/security.inc.php
@@ -0,0 +1,114 @@
+<?php
+/*
+  RoxyFileman - web based file manager. Ready to use with CKEditor, TinyMCE. 
+  Can be easily integrated with any other WYSIWYG editor or CMS.
+
+  Copyright (C) 2013, RoxyFileman.com - Lyubomir Arsov. All rights reserved.
+  For licensing, see LICENSE.txt or http://RoxyFileman.com/license
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Contact: Lyubomir Arsov, liubo (at) web-lobby.com
+*/
+
+
+
+//var_dump($includesDir . 'global.php');
+
+if (!function_exists('http_response_code')) {
+  function http_response_code($code = NULL) {
+
+    if ($code !== NULL) {
+
+      switch ($code) {
+        case 100: $text = 'Continue'; break;
+        case 101: $text = 'Switching Protocols'; break;
+        case 200: $text = 'OK'; break;
+        case 201: $text = 'Created'; break;
+        case 202: $text = 'Accepted'; break;
+        case 203: $text = 'Non-Authoritative Information'; break;
+        case 204: $text = 'No Content'; break;
+        case 205: $text = 'Reset Content'; break;
+        case 206: $text = 'Partial Content'; break;
+        case 300: $text = 'Multiple Choices'; break;
+        case 301: $text = 'Moved Permanently'; break;
+        case 302: $text = 'Moved Temporarily'; break;
+        case 303: $text = 'See Other'; break;
+        case 304: $text = 'Not Modified'; break;
+        case 305: $text = 'Use Proxy'; break;
+        case 400: $text = 'Bad Request'; break;
+        case 401: $text = 'Unauthorized'; break;
+        case 402: $text = 'Payment Required'; break;
+        case 403: $text = 'Forbidden'; break;
+        case 404: $text = 'Not Found'; break;
+        case 405: $text = 'Method Not Allowed'; break;
+        case 406: $text = 'Not Acceptable'; break;
+        case 407: $text = 'Proxy Authentication Required'; break;
+        case 408: $text = 'Request Time-out'; break;
+        case 409: $text = 'Conflict'; break;
+        case 410: $text = 'Gone'; break;
+        case 411: $text = 'Length Required'; break;
+        case 412: $text = 'Precondition Failed'; break;
+        case 413: $text = 'Request Entity Too Large'; break;
+        case 414: $text = 'Request-URI Too Large'; break;
+        case 415: $text = 'Unsupported Media Type'; break;
+        case 500: $text = 'Internal Server Error'; break;
+        case 501: $text = 'Not Implemented'; break;
+        case 502: $text = 'Bad Gateway'; break;
+        case 503: $text = 'Service Unavailable'; break;
+        case 504: $text = 'Gateway Time-out'; break;
+        case 505: $text = 'HTTP Version not supported'; break;
+        default:
+          exit('Unknown http status code "' . htmlentities($code) . '"');
+          break;
+      }
+
+      $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+
+      header($protocol . ' ' . $code . ' ' . $text);
+
+      $GLOBALS['http_response_code'] = $code;
+
+    } else {
+
+      $code = (isset($GLOBALS['http_response_code']) ? $GLOBALS['http_response_code'] : 200);
+
+    }
+
+    return $code;
+
+  }
+}
+
+//die("foo");
+function checkAccess($action){
+
+  require_once HELPDESKZ_ROOT . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'global.php';
+  require_once HELPDESKZ_ROOT . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'staff.inc.php';
+
+  if($staff['status'] != 'Enable'){
+
+    setcookie('stfhash','',time()-5,'/');
+    unset($_SESSION['staff']);
+
+    http_response_code(401);
+
+    exit;
+  }
+  if(in_array($staff['timezone'], $timezone)){
+    date_default_timezone_set($staff['timezone']);
+  }
+
+  return true;
+}
+?>
