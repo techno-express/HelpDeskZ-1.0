6a882c06becdf188030597ad341a52f20a88c2e2
Peter Notenboom <peter@digiter.nl>
2016-03-22 13:42:58 +0100

    Dashboard trash count() fix

--- controllers/staff/dashboard_action.php
+++ controllers/staff/dashboard_action.php
@@ -22,7 +22,7 @@ while($r = $db->fetch_array($q)){
 		$exceptiondep_query .= " AND department_id!={$r['id']}";
 	}
 }
-$tickets_summary = $db->fetchRow("SELECT (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE status='1' {$exceptiondep_query}) AS open, (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE status='2' {$exceptiondep_query}) as answered, (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE status='3' {$exceptiondep_query}) as awaiting_reply, (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE status='4' {$exceptiondep_query}) as in_progress, (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE status='5' {$exceptiondep_query}) as closed");
+$tickets_summary = $db->fetchRow("SELECT (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE trash = 0 AND status='1' {$exceptiondep_query}) AS open, (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE trash = 0 AND status='2' {$exceptiondep_query}) as answered, (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE trash = 0 AND status='3' {$exceptiondep_query}) as awaiting_reply, (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE trash = 0 AND status='4' {$exceptiondep_query}) as in_progress, (SELECT COUNT(id) FROM ".TABLE_PREFIX."tickets WHERE trash = 0 AND status='5' {$exceptiondep_query}) as closed");
 $template_vars['lastestnews'] = $lastestnews;
 $template_vars['login_log'] = $login_log;
 $template_vars['tickets_summary'] = $tickets_summary;
