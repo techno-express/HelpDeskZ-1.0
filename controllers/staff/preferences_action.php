39cde0b775e63e132214c247af11e3593b25de6a
Peter Notenboom <peter@digiter.nl>
2016-03-31 11:38:53 +0200

    Preferences change password fix

--- controllers/staff/preferences_action.php
+++ controllers/staff/preferences_action.php
@@ -7,12 +7,11 @@
  * @since 1.0.0
  */
 if($params[0] == 'update_password'){
-	print_r($staff);
 	if(verifyToken('preferences', $input->p['csrfhash']) !== true){
 		$error_msg = $LANG['CSRF_ERROR'];
 	}elseif(empty($input->p['current_password']) || empty($input->p['new_password']) || empty($input->p['new_password2'])){
 		$error_msg = $LANG['ONE_REQUIRED_FIELDS_EMPTY'];
-	}elseif( Password::isEqual($staff['new_password'], $input->p['current_password']) === false ){
+	}elseif( Password::isEqual($staff['password'], $input->p['current_password']) === false ){
 		$error_msg = $LANG['EXISTING_PASSWORD_INCORRECT'];
 	}elseif($input->p['new_password'] != $input->p['new_password2']){
 		$error_msg = $LANG['NEW_PASSWORDS_DONOT_MATCH'];
